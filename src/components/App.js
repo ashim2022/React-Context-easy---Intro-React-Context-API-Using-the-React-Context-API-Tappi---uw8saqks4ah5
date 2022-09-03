6d79fc23a925a3ccbb490f54c2bad05931150e2a
Ashim Shaw <shawashim72@gmail.com>
2022-09-03 21:52:11 +0530

    files

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import '../styles/App.css';
-import { UserProfile } from './UserProfile';
-
-
+import React from "react";
+import "../styles/App.css";
+import { UserProfile } from "./UserProfile";
+import { createContext } from "react";
+const UserContext = createContext();
 const App = () => {
-
   return (
     <div id="main">
-      
+      <UserContext.Provider value={{name:"Newton",age:3}}>
         <UserProfile />
-      
+      </UserContext.Provider>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
-export {UserContext}
+export { UserContext };
